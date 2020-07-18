b95834b89c36389119657262537b6c7eb6b0d3e8
Avengers672 <67515955+Avengers672@users.noreply.github.com>
2020-07-18 20:22:15 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,27 @@
+function Summer(){
+    alert("Hey! it's SUMMER!");
+    document.body.style.backgroundImage= "url('https://res.cloudinary.com/pavie/image/upload/v1595005653/summer_jt8yiy.png')";
+    document.body.style.backgroundSize = "100%";
+    document.body.style.backgroundRepeat = "no-repeat";
+}
+function Winter(){
+    alert("Hey! it's WINTER!");
+    document.body.style.backgroundImage= "url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')";
+    document.body.style.backgroundSize = "100%";
+    document.body.style.backgroundRepeat = "no-repeat";
+}
+function Autumn(){
+    alert("Hey! it's Autmn");
+    document.body.style.backgroundImage= "url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')";
+    document.body.style.backgroundSize = "100%";
+    document.body.style.backgroundRepeat = "no-repeat";
+}
+    function Spring(){
+    alert("Hey! it's SPRING!");
+    document.body.style.backgroundImage= "url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')";
+    document.body.style.backgroundSize = "100%";
+    document.body.style.backgroundRepeat = "no-repeat";
+}
+function copy(){
+    alert("lol! you can't copy!");
+}
